f21ecb291ffc77d1c44dc07829a0a95da4b59c9f
Pascal Chambon <pythoniks@gmail.com>
2021-03-10 11:03:32 +0100

    Fix some variable names

--- wa_keygen_gui.py
+++ wa_keygen_gui.py
@@ -27,34 +27,37 @@ from wacryptolib.key_storage import FilesystemKeyStorage
 
 from wacryptolib.utilities import generate_uuid0
 
-
+'''
 class MyCheckbox(IRightBodyTouch, MDCheckbox):
     pass
 
 
 class MyAvatar(ILeftBody, Image):
     pass
-
+'''
 
 THREAD_POOL_EXECUTOR = ThreadPoolExecutor(
     max_workers=1, thread_name_prefix="keygen_worker"  # SINGLE worker for now, to avoid concurrency
 )
+
+GENERATED_KEYS_COUNT = 7
 
 
 class MainApp(MDApp):
 
     kv_file = "wa_keygen_gui.kv"
 
-    list_devices = ()
+    device_list = ()
+    keygen_panel = None
 
     def __init__(self, **kwargs):
         self.title = "Witness Angel - WardProject"
         super(MainApp, self).__init__(**kwargs)
 
     def get_form_values(self):
-        return dict(user=self.list.ids.userfield.text.strip(),
-                    passphrase=self.list.ids.passphrasefield.text.strip(),
-                    passphrase_hint=self.list.ids.passphrasehintfield.text.strip())
+        return dict(user=self.keygen_panel.ids.userfield.text.strip(),
+                    passphrase=self.keygen_panel.ids.passphrasefield.text.strip(),
+                    passphrase_hint=self.keygen_panel.ids.passphrasehintfield.text.strip())
 
 
     def show_validate(self):
@@ -83,12 +86,12 @@ class MainApp(MDApp):
     def close_dialog(self, obj):
         self.dialog.dismiss()
 
-    def get_detected_devices(self):
-        self.list = Factory.Lists()
-        list_devices = list_available_authentication_devices()
-        self.list_devices = list_devices
+    def list_detected_devices(self):
+        self.keygen_panel = Factory.KeygenPanel()
+        device_list = list_available_authentication_devices()
+        self.device_list = device_list
 
-        for index, usb in enumerate(list_devices):
+        for index, usb in enumerate(device_list):
             self.lineCheck = Factory.ListItemWithCheckbox(
                 text="[color=#FFFFFF][b]Path:[/b] %s[/color]" % (str(usb["path"])),
                 secondary_text="[color=#FFFFFF][b]Label:[/b] %s[/color]" % (str(usb["label"])),
@@ -96,23 +99,18 @@ class MainApp(MDApp):
 
             )
             self.lineCheck.bind(on_release=self.get_info_key_selected)
-            self.list.ids.scroll.add_widget(self.lineCheck)
-        self.screen.add_widget(self.list)
-
-
-    def refresh_list(self):
-        self.get_detected_devices()
+            self.keygen_panel.ids.scroll.add_widget(self.lineCheck)
+        self.screen.add_widget(self.keygen_panel)
 
     def _offloaded_initialize_rsa_key(self, form_values):
 
         success = False
 
         try:
-            keys_count = 7
+
             #print(">starting initialize_rsa_key")
 
             Clock.schedule_once(partial(self._do_update_progress_bar, 10))
-
 
             initialize_authentication_device(self.authentication_device_selected,
                                              user=form_values["user"],
@@ -122,7 +120,7 @@ class MainApp(MDApp):
 
             object_FilesystemKeyStorage = FilesystemKeyStorage(key_storage_folder)
 
-            for i in range(1, keys_count+1):
+            for i in range(1, GENERATED_KEYS_COUNT+1):
                 #print(">WIP initialize_rsa_key", id)
                 key_pair = generate_asymmetric_keypair(
                     key_type="RSA_OAEP",
@@ -135,7 +133,7 @@ class MainApp(MDApp):
                     private_key=key_pair["private_key"],
                 )
 
-                Clock.schedule_once(partial(self._do_update_progress_bar, 10 + int (i * 90 / keys_count)))
+                Clock.schedule_once(partial(self._do_update_progress_bar, 10 + int (i * 90 / GENERATED_KEYS_COUNT)))
 
             success = True
 
@@ -144,23 +142,22 @@ class MainApp(MDApp):
 
         Clock.schedule_once(partial(self.finish_initialization, success=success))
 
-
     def build(self):
         self.authentication_device_selected = None
         self.orientation = "vertical"
         self.screen = Screen()
-        self.get_detected_devices()
+        self.list_detected_devices()
         return self.screen
 
     def get_info_key_selected(self, linelist):
-        list_ids=self.list.ids
+        list_ids=self.keygen_panel.ids
         text_fields = [
             list_ids.userfield,
             list_ids.passphrasefield,
             list_ids.passphrasehintfield,
         ]
 
-        list_devices = self.list_devices
+        device_list = self.device_list
         for i in list_ids.scroll.children:
             i.bg_color = [0.1372, 0.2862, 0.5294, 1]
         linelist.bg_color = [0.6, 0.6, 0.6, 1]
@@ -180,7 +177,7 @@ class MainApp(MDApp):
         list_ids.label_alert.clear_widgets()
         list_ids.labelInfoUsb1.add_widget(self.l)
         list_ids.label_alert.add_widget(self.alertMessage)
-        for index, authentication_device in enumerate(list_devices):
+        for index, authentication_device in enumerate(device_list):
             if linelist.text == "[color=#FFFFFF][b]Path:[/b] " + str(authentication_device["path"]) + "[/color]":  # FIXME
                 self.authentication_device_selected = authentication_device
                 if authentication_device["is_initialized"]:
@@ -220,30 +217,29 @@ class MainApp(MDApp):
         Clock.schedule_once(partial(self._do_update_progress_bar, percent))
 
     def _do_update_progress_bar(self, percent, *args, **kwargs):
-        #print(">>>>>>", self.list.ids)
-        self.list.ids.barProgress.value = percent
-
+        #print(">>>>>>", self.keygen_panel.ids)
+        self.keygen_panel.ids.barProgress.value = percent
 
     def initialize(self, form_values):
         self.l.text = "Please wait a few seconds."
         self.alertMessage.text = "The operation is being processed."
-        self.list.ids.btn_refresh.disabled = True
-        self.list.ids.button_initialize.disabled = True
-        self.list.ids.passphrasefield.text = "***"  # PRIVACY
-        for c in list(self.list.ids.scroll.children):
+        self.keygen_panel.ids.btn_refresh.disabled = True
+        self.keygen_panel.ids.button_initialize.disabled = True
+        self.keygen_panel.ids.passphrasefield.text = "***"  # PRIVACY
+        for c in list(self.keygen_panel.ids.scroll.children):
             c.bg_color=[1, 1, 1, 0.4]
-        #self.list.ids.scroll.bg_color=[1, 0.2862, 0.5294, 1]
+        #self.keygen_panel.ids.scroll.bg_color=[1, 0.2862, 0.5294, 1]
         self.lineCheck.unbind(on_release=self.get_info_key_selected)
 
         THREAD_POOL_EXECUTOR.submit(self._offloaded_initialize_rsa_key, form_values=form_values)
 
     def finish_initialization(self, *args, success, **kwargs):
 
-        self.list.ids.btn_refresh.disabled = False
+        self.keygen_panel.ids.btn_refresh.disabled = False
         self._do_update_progress_bar(0)  # Reset
 
         if success:
-            #self.list.ids.errors.add_widget(
+            #self.keygen_panel.ids.errors.add_widget(
             #    Label(text="Initialization successful", font_size="28sp", color=[0, 1, 0, 1])
             #)
 
@@ -252,6 +248,7 @@ class MainApp(MDApp):
         else:
             self.l.text = "Errors occurred"
             self.alertMessage.text = "Operation failed, check logs"
+
 
 def resourcePath():
     '''Returns path containing content - either locally or in pyinstaller tmp file'''
